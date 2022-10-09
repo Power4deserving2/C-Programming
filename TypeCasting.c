5a04b135115e5c38f10ef3f5abdd3a2e50e39074
Immanuel Peter <ipmaster40@gmail.com>
2022-10-09 11:12:14 +0300

    Type casting and quizzes

---
+++ TypeCasting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int slices;
+	double halfThePizza = slices / 2;
+	printf("%f\n", halfThePizza);
+
+	double c = 25/2*2;
+	double d = 25/2*2.0;
+	printf("C: %f\n",c);
+	printf("D: %f\n",d);
+	return 0;
+}
